b9aad8e3c4c0e08ca56fc8e94cc17508d66f5b4c
jeremy.degano@gmail.com <jeremy.degano@gmail.com>
2022-02-04 20:09:48 +0100

    Put background and add img bush

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ pygame.display.set_caption("DynoLand")
 screen = pygame.display.set_mode((1080,720))
 
 isRunning = True
+clock = pygame.time.Clock()
 
 #initialize background
-#background = pygame.image.load()
+background = pygame.image.load('assets/background.jpg')
 
 #Boucle infini du jeu
 while isRunning:
+    clock.tick(30)   
+
+    #initialize background
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
     #Verif if player shut the game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             isRunning = False
             pygame.quit()
-            print('the game as been close')
